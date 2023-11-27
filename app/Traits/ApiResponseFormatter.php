a642c43d58824c56a9e6b767c0a2f70ecb66f44e
Rozi85 <rozifatkhur85@gmail.com>
2023-11-27 22:26:25 +0700

    upload tugas

---
+++ app/Traits/ApiResponseFormatter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\Traits;
+
+trait ApiResponseFormatter
+{
+    public function apiResponse($code =200, $message = "succes", $data = [])
+    {
+        return json_encode([
+            "code" => $code,
+            "message" => $message,
+            "data" => $data
+        ]);
+    }
+}
